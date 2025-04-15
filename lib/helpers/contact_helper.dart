4fee559deb3e8ad53848cc35195bef8630356a52
Manrick-Lazaro <manricksantos@gmail.com>
2025-04-15 11:19:20 -0300

    feat: remove service into contact_helper.dart.

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -1,65 +1,12 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
-final String contactTable = 'contactTable';
-final String idColumn = 'idColumn';
-final String nameColumn = 'nameColumn';
-final String emailColumn = 'emailColumn';
-final String phoneColumn = 'phoneColumn';
-final String imageColumn = 'imageColumn';
-
-class ContactHelper {
-  static final ContactHelper _instance = ContactHelper.internal();
-
-  factory ContactHelper() => _instance;
-
-  ContactHelper.internal();
-
-  late Database _db;
-
-  Future<Database> get db async => _db = await initDb();
-
-  Future<Database> initDb() async {
-    final databasePath = await getDatabasesPath();
-    final path = join(databasePath, 'contacts.db');
-
-    return await openDatabase(
-      path,
-      version: 1,
-      onCreate: (Database db, int newerversion) async {
-        await db.execute(
-          "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, "
-          "$emailColumn TEXT, $phoneColumn TEXT, $imageColumn TEXT)",
-        );
-      },
-    );
-  }
-
-  Future<Contact> saveContact(Contact contact) async {
-    Database dbConnection = await db;
-
-    contact.id = await dbConnection.insert(contactTable, contact.toMap());
-
-    return contact;
-  }
-
-  Future<Contact?> getContact(int id) async {
-    Database dbConnection = await db;
-
-    List<Map> maps = await dbConnection.query(
-      contactTable,
-      columns: [idColumn, nameColumn, emailColumn, phoneColumn, imageColumn],
-      where: "$idColumn = ?",
-      whereArgs: [id],
-    );
-
-    if(maps.isNotEmpty) {
-      return Contact.fromMap(maps[0]);
-    } else {
-      return null;
-    }
-  }
-}
+final String _contactTable = 'contactTable';
+final String _idColumn = 'idColumn';
+final String _nameColumn = 'nameColumn';
+final String _emailColumn = 'emailColumn';
+final String _phoneColumn = 'phoneColumn';
+final String _imageColumn = 'imageColumn';
 
 class Contact {
   late int id;
@@ -68,21 +15,22 @@ class Contact {
   late String phone;
   late String image;
 
+  Contact();
+
   Contact.fromMap(Map map) {
-    id = map[idColumn];
-    name = map[nameColumn];
-    email = map[emailColumn];
-    phone = map[phoneColumn];
-    image = map[imageColumn];
+    id = map[_idColumn];
+    name = map[_nameColumn];
+    email = map[_emailColumn];
+    phone = map[_phoneColumn];
+    image = map[_imageColumn];
   }
 
   Map<String, dynamic> toMap() {
     Map<String, dynamic> map = {
-      idColumn: id,
-      nameColumn: name,
-      emailColumn: email,
-      phoneColumn: phone,
-      imageColumn: image,
+      _nameColumn: name,
+      _emailColumn: email,
+      _phoneColumn: phone,
+      _imageColumn: image,
     };
     return map;
   }
